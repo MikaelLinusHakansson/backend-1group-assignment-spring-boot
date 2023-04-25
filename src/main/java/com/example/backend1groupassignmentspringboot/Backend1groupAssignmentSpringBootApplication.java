134dab2874edaa60772499a5d6a1ffddda851e49
Linus Håkansson <Linus.Hakansson@yh.nackademin.se>
2023-04-25 17:33:51 +0200

    created the entity varor, added and finished the controller with all the endpoints, made test for the controller endpoints and added data to the database.

--- src/main/java/com/example/backend1groupassignmentspringboot/Backend1groupAssignmentSpringBootApplication.java
+++ src/main/java/com/example/backend1groupassignmentspringboot/Backend1groupAssignmentSpringBootApplication.java
@@ -1,13 +1,35 @@
 package com.example.backend1groupassignmentspringboot;
 
+import com.example.backend1groupassignmentspringboot.dao.VarorRepository;
+import com.example.backend1groupassignmentspringboot.entity.Varor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class Backend1groupAssignmentSpringBootApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Backend1groupAssignmentSpringBootApplication.class, args);
+    }
+
+    @Bean
+    CommandLineRunner demo(VarorRepository varorRepository){
+        return (args) -> {
+            Varor vara1 = new Varor("Kaffe", 200);
+            Varor vara2 = new Varor("Skor", 199.99);
+            Varor vara3 = new Varor("Strumpor", 20.99);
+
+            varorRepository.save(vara1);
+            varorRepository.save(vara2);
+            varorRepository.save(vara3);
+
+
+        };
     }
 
 }
